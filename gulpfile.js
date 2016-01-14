3ff7af0588f2fda69953446e7a5f8b915623f4d8
giorgiopea <annatar93@gmail.com>
2016-01-14 21:33:30 +0100

    added desc

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sync = require('browser-sync');
-var connect = require('gulp-connect');
 
 
 //TODO fix this
@@ -16,14 +15,5 @@ gulp.task('broswer-sync', function () {
         }
     });
 });
-gulp.task('watch', function () {
-    gulp.watch('assets/sass/**/*.sass', ['sass_compile']);
-    gulp.watch('app/*/*/*/*.js', ['concat']);
-});
 
-
-gulp.task('dev', ['watch', 'broswer-sync', 'sass_compile']);
-
-gulp.task('default', ['watch', 'broswer-sync', 'sass_compile'], function () {
-
-});
+gulp.task('default', ['broswer-sync']);
